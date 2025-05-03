d501d41446d3235122b4f2b8a01495d01c18812f
Benjamin Hammerle <hb6912@mci4me.at>
2025-05-03 21:25:12 +0200

    Init: test 8

--- src/main/java/com/mci/swe/base/ui/view/MainView.java
+++ src/main/java/com/mci/swe/base/ui/view/MainView.java
@@ -20,7 +20,7 @@ public final class MainView extends Main {
         addClassName(LumoUtility.Padding.MEDIUM);
         add(new ViewToolbar("Main"));
         add(new Div("Please select a view from the menu on the left."));
-        add(new Div("Here will grow a wonderful app."));
+        add(new Div("Here will grow a wonderful app!!!"));
     }
 
     /**
